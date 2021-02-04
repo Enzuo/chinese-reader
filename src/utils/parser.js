8db280214180bc5c91d695c01a3b875eb71ea7da
Enzuo <potc.zone+git@gmail.com>
2021-02-04 15:45:12 +0100

    WIP text parser

--- src/utils/parser.js
+++ src/utils/parser.js
@@ -4,20 +4,10 @@ import dict from '../dictionary/mandarin_words.json'
 
 export function parseText (text) {
   const parsedText = []
-  var sentences = []
-  if(typeof text === 'string'){
-    sentences = text.split(/([.|\n|。|,|，])/g)
+  var sentences = typeof text === 'string' ? splitSentences(text) : text
+  for (var i=0; i<sentences.length; i++){
+    parseSentence(sentences[i]);
   }
-  for(var i=0; i< sentences.length; i++){
-    var st = sentences[i];
-    if(st === '.' || st === '\n' || st === '。'|| st  === ','|| st === '，'){
-      parsedText.push(st)
-      continue
-    }
-    var parsedSentence = parseSentence(st)
-    parsedText.push(parsedSentence)
-  }
-  return parsedText;
 }
 
 export function splitSentences (text) {
@@ -40,30 +30,50 @@ export function splitSentences (text) {
   return sentences
 }
 
+// input  。你有 想要 购买镜头的冲动与欲望。\n
+// output 。你 有 想要 购买 镜头 的 冲动 与 欲望。\n
 export function parseSentence (sentence) {
-  var parsedSentence = []
-  var maxChunkSize = 5
-  var chunkSize
-  for(var i=0; i<sentence.length; i += chunkSize){
-    chunkSize = maxChunkSize
-    var chunk
-    var isFound = false
-    while(!isFound && chunkSize > 1){
-      chunk = sentence.substr(i, chunkSize)
-      // chunkSize = chunk.length // for the end of the sentence take maximum chunck size // infinit loop
-      console.log('lookingFor chunk in dictionary', chunk, chunkSize)
-      var index = dict.findIndex(entry => entry === chunk)
-      if(index >= 0){
-        isFound = true
-      }
-      else {
-        chunkSize--
-      }
-    }
-    chunk = sentence.substr(i, chunkSize)
-    parsedSentence.push(chunk)
+  var parserIndex = 0;
+  while(parserIndex < sentence.length){
+    getCharacterChunk(parserIndex, sentence)
   }
-  console.log(parsedSentence)
+  // var parsedSentence = []
+  // var maxChunkSize = 5
+  // var chunkSize
+  // for(var i=0; i<sentence.length; i += chunkSize){
+  //   chunkSize = maxChunkSize
+  //   var chunk
+  //   var isFound = false
+  //   while(!isFound && chunkSize > 1){
+  //     chunk = sentence.substr(i, chunkSize)
+  //     // chunkSize = chunk.length // for the end of the sentence take maximum chunck size // infinit loop
+  //     console.log('lookingFor chunk in dictionary', chunk, chunkSize)
+  //     var index = dict.findIndex(entry => entry === chunk)
+  //     if(index >= 0){
+  //       isFound = true
+  //     }
+  //     else {
+  //       chunkSize--
+  //     }
+  //   }
+  //   chunk = sentence.substr(i, chunkSize)
+  //   parsedSentence.push(chunk)
+  // }
+  // console.log(parsedSentence)
+}
+
+function getCharacterChunk (index, text) {
+  const maxChunkSize = 5;
+  var chunk = text.slice(index, index + maxChunkSize);
+
+  var isSpecialChar = false;
+  for(var i=0; i < chunk.length; i++){
+    var char = chunk[i];
+    if(char.match(/\.|\n|。|,|，/)){
+      isSpecialChar = true;
+      continue
+    }
+  }
 }
 
 export default { parseText, splitSentences, parseSentence }
--- src/utils/parser.test.js
+++ src/utils/parser.test.js
@@ -1,11 +1,18 @@
 import parser from './parser'
 
-test('exytact sentences', () => {
-  var text = `
+test('extract sentences', () => {
+  const text = `
   你有想要购买镜头的冲动与欲望, 却又因为经济拮据
   而迟迟无法下手吗。
   `
-  var parsedText = parser.splitSentences(text)
-  console.log(parsedText);
+  const parsedText = parser.splitSentences(text)
+  console.log(parsedText)
   // expect(text).toBe('hello')
 });
+
+
+test('parse sentence', () => {
+  const sentence = '。你有想要购买镜头的冲动与欲望。\n'
+  const parsedSentence = parser.parseSentence(sentence)
+  console.log(parsedSentence);
+})
